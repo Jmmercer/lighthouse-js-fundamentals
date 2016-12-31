b50c0d2df52a9a65f315a128a3489350a98c10da
Jeffrey <Jmmercer@ucalgary.ca>
2016-12-31 04:43:27 +0000

    adding odd

---
+++ odd.js
@@ -0,0 +1,9 @@
+function isOdd(num) {
+  return num % 2 === 1;
+}
+console.log("3 is odd: " + isOdd(3));
+console.log("8 is odd: " + isOdd(8));
+/*for ( i = 0; i < 10; i++) {
+  console.log(isOdd(i))
+}
+*/
