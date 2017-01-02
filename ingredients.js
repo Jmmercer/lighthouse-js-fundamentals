83d0a314f63d1f746f21632a93a12f116c3f95db
Jeffrey <Jmmercer@ucalgary.ca>
2017-01-02 18:55:08 +0000

    added ingredients

---
+++ ingredients.js
@@ -0,0 +1,15 @@
+var ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+/*var bakingList = 0
+
+while  (bakingList < ingredients.length) {
+  console.log(ingredients);
+  ingredients++;
+}
+
+for (var i = 0; i < ingredients.length; i++) {
+  console.log(ingredients[i]);
+}
+*/
+for (var i = ingredients.length; i >= 0; i--) {
+  console.log(ingredients[i]);
+}
