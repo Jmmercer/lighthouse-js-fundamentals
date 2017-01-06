bfc523a7b0b3738a202fa868f802bc709f2f41db
Jeffrey <Jmmercer@ucalgary.ca>
2017-01-06 04:10:49 +0000

    refractloopylighthouse complete

---
+++ refactloopy.js
@@ -0,0 +1,12 @@
+function loopyLighthouse([range, range2], [multiples, multiples2], [word, word2]){
+  for (var i = range; i < range2 +1; i++){
+        if (i % multiples === 0 && i % multiples2 === 0){
+      console.log(word + word2);
+    } else if (i % multiples === 0){
+      console.log(word);
+    } else if (i % multiples2 === 0){
+      console.log(word2);
+    } else console.log(i);
+  }
+}
+loopyLighthouse([15, 90], [2, 5], ["Batty", "Beacon"]);
