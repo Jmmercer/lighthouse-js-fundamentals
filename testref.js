e3c4e7a3de1f8931cfa9f4656b72a251fab3b415
Jeffrey <Jmmercer@ucalgary.ca>
2017-01-10 01:26:54 +0000

    updated

--- testref.js
+++ testref.js
@@ -1,14 +1,11 @@
-function loopyLighthouse(range, multiples, word){
-  range = [range, range2]
-  multiples = [multiples, multiples2]
-  word = [word, word2]
-  for (var i = range; i < range2 +1; i++){
-        if (i % multiples === 0 && i % multiples2 === 0){
-      console.log(word + word2);
-    } else if (i % multiples === 0){
-      console.log(word);
-    } else if (i % multiples2 === 0){
-      console.log(word2);
+function loopyLighthouse(range, multiples, words){
+  for (var i = range[0]; i < range[1] +1; i++){
+        if (i % multiples[0] === 0 && i % multiples[1] === 0){
+      console.log(words[0] + words[1]);
+    } else if (i % multiples[0] === 0){
+      console.log(words[0]);
+    } else if (i % multiples[1] === 0){
+      console.log(words[1]);
     } else console.log(i);
   }
 }
